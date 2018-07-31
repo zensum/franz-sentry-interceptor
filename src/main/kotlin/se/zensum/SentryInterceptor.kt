3cbdb127b12e4c194f1008aa4827c4ce7624c067
Bona <bjorn.fyrvall@gmail.com>
2018-07-31 16:11:51 +0200

    Added info logging when an exception is captured.

--- src/main/kotlin/se/zensum/SentryInterceptor.kt
+++ src/main/kotlin/se/zensum/SentryInterceptor.kt
@@ -3,8 +3,11 @@ package se.zensum.franzSentry
 import franz.JobStatus
 import franz.WorkerInterceptor
 import io.sentry.Sentry
+import mu.KotlinLogging
 
 fun getEnv(e : String, default: String? = null) : String = System.getenv()[e] ?: default ?: throw RuntimeException("Missing environment variable $e and no default value is given.")
+
+private val log = KotlinLogging.logger("sentry")
 
 class SentryInterceptor(
     dsn: String? = getEnv("SENTRY_DSN", null),
@@ -13,6 +16,7 @@ class SentryInterceptor(
         try{
             interceptor.executeNext(default)
         }catch (e: Throwable){
+            log.info { e.message }
             Sentry.capture(e)
             throw e
         }finally {
